179d68980372758bf1b43a930f9d78abae215359
Soksan369 <147368894+Soksan369@users.noreply.github.com>
2025-04-03 20:40:24 +0700

    init activity firebase

---
+++ FLUTTER-FIREBASE-STUDENT/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/students_provider.dart';  
+import 'repositories/student_repository.dart'; 
+import 'screens/home_screen.dart'; 
+import 'repositories/firebase_students_repository.dart'; 
+
+void main() {
+  // Instantiate the correct repository for students
+  final StudentRepository repository = FirebaseStudentsRepository(); 
+  
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => StudentsProvider(repository),  
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
+    ),
+  );
+}
